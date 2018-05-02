53581fb04f559641b351673d550990dd0bfe63e1
Albert Zarate <thealbertzarate@gmail.com>
2018-05-02 10:41:02 -0700

    done with code

--- fcfscode.cpp
+++ fcfscode.cpp
@@ -1,79 +1,125 @@
+// FCFS Scheduling Algorithm
 #include<iostream>
-#include <stdio.h>      /* printf, scanf, puts, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include <stdio.h>      
+#include <stdlib.h>
+#include <time.h>  
 using namespace std;
 
-// Driver code
+
+//waittime = startime[n+1] - arrival
+//turnaroundtime = waittime + bursttime
+
+
+void findWaitTime(int n, int burstTime[], int waitTime[], int arrivalTime[]);
+void findTurnaroundTime(int turnaroundTime[], int waitTime[],int burstTime[], int n);
+int findAvgWaitTime(int waitTime[], int n);
+int findAvgTurnaroundTime(int turnaroundTime[], int n);
+void swap(int* a, int* b);
+int partition (int arr1[], int arr2[], int arr3[], int low, int high);
+void quickSort(int arr1[], int arr2[], int arr3[], int low, int high);
+
 int main(){
-    srand (time(NULL));
-    int processes[] = {1, 2, 3};
-    int n = 7;//random number between 0-99
-    int burst_time[] = {rand()%100, rand()%100, rand()%100};
-    int arrival_time[] = {rand()%1000, rand()%1000, rand()%1000};
-    calcAvgTime(processes, n, burst_time, arrival_time);
+    int process_size[] = {50, 100, 500};
+    cout<<"N \t\tRun number\t\tAvg. waiting time\t\tAvg. turnaround time"<<endl;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 5; j++){
+            int amount = process_size[i];
+            srand (j);
+            int processes[amount];
+            int burstTime[amount];
+            int arrivalTime[amount];
+            int waitTime[amount];
+            int turnaroundTime[amount];
+            int avgWaitTime = 0;
+            int avgTurnaroundTime = 0;
+            for(int i = 0; i < amount; i++){
+                processes[i] = i+1;
+                burstTime[i] = rand() % 100 +1;
+                arrivalTime[i] = rand() % 1000 +1;
+            }
+            quickSort(arrivalTime, burstTime, processes, 0, amount-1);
+            cout<<amount<<"\t\t"<<j+1<<"\t\t\t\t";
+            findWaitTime(amount, burstTime, waitTime, arrivalTime);
+            findTurnaroundTime(turnaroundTime, waitTime, burstTime, amount);
+            avgWaitTime = findAvgWaitTime(waitTime, amount);
+            avgTurnaroundTime = findAvgTurnaroundTime(turnaroundTime, amount);
+            if(amount==50)
+                cout<<(float)avgWaitTime<<"\t\t\t\t\t\t"<<(float)avgTurnaroundTime<<endl;
+            else
+                cout<<(float)avgWaitTime<<"\t\t\t\t\t"<<(float)avgTurnaroundTime<<endl;
+        }
+    }
     return 0;
 }
 
- // Function to calculate average waiting and turn-around
-// times.
-void calcAvgTime(int processes[], int n, int burstTime[], int accessTime[]){
-    int waitTime[n], turnAroundTime[n];
- 
-    // Function to find waiting time of all processes
-    calcWaitingTime(processes, n, burstTime, waitTime, accessTime);
- 
-    // Function to find turn around time for all processes
-    calcTurnAroundTime(processes, n, burstTime, waitTime, turnAroundTime);
- 
-    // Display processes along with all details
-    cout << "Processes " << " Burst Time " << " Arrival Time "
-         << " Waiting Time " << " Turn-Around Time "
-         << " Completion Time \n";
-    int totalWaitTime = 0, totalTurnAroundTime = 0;
-    for (int i = 0 ; i < n ; i++)
-    {
-        totalWaitTime = totalWaitTime + waitTime[i];
-        totalTurnAroundTime = totalTurnAroundTime + turnAroundTime[i];
-        int completionTime = turnAroundTime[i] + accessTime[i];
-        cout << " " << i+1 << "\t\t" << burstTime[i] << "\t\t"
-             << accessTime[i] << "\t\t" << waitTime[i] << "\t\t "
-             << turnAroundTime[i]  <<  "\t\t " << completionTime << endl;
+void findWaitTime(int n, int burstTime[], int waitTime[], int arrivalTime[]){
+    int startTime[n];
+    startTime[0]=arrivalTime[0];
+    waitTime[0]=0;
+    for (int i = 1; i < n; ++i){
+        if (arrivalTime[i]<startTime[i-1]+burstTime[i-1]){
+            startTime[i]=startTime[i-1]+burstTime[i-1];
+            waitTime[i]=startTime[i]-arrivalTime[i];
+        }
+        else{
+            startTime[i]=arrivalTime[i];
+            waitTime[i]=0;
+        }
     }
- 
-    cout << "Average waiting time = "
-         << (float)totalWaitTime / (float)n;
-    cout << "\nAverage turn around time = "
-         << (float)totalTurnAroundTime / (float)n;
 }
- 
 
-// Function to find the waiting time for all processes
-void calcWaitTime(int processes[], int n, int burstTime[], int waitTime[], int accessTime[]){
-    int serviceTime[n];
-    serviceTime[0] = 0;
-    waitTime[0] = 0;
- 
-    // calculating waiting time
-    for (int i = 1; i < n ; i++){
-        // Add burst time of previous processes
-        serviceTime[i] = serviceTime[i-1] + burstTime[i-1];
- 
-        // Find waiting time for current process =
-        // sum - at[i]
-        waitTime[i] = serviceTime[i] - accessTime[i];
- 
-        // If waiting time for a process is in negative
-        // that means it is already in the ready queue
-        // before CPU becomes idle so its waiting time is 0
-        if (waitTime[i] < 0) waitTime[i] = 0;
+void findTurnaroundTime(int turnaroundTime[], int waitTime[],int burstTime[], int n){
+    for (int i = 0; i < n; ++i){
+        turnaroundTime[i]=waitTime[i] + burstTime[i];
     }
 }
- 
-// Function to calculate turn around time
-void calcTurnAroundTime(int processes[], int n, int burstTime[], int waitTime[], int turnAroundTime[]){
-    // Calculating turnaround time by adding burstTime[i] + waitTime[i]
-    for (int i = 0; i < n ; i++)
-        turnAroundTime[i] = burstTime[i] + waitTime[i];
+
+int findAvgWaitTime(int waitTime[], int n){
+    int sum = 0;
+    int average = 0;
+    for (int i = 0; i < n; i++) {
+        sum += waitTime[i];
+    }
+    return average = sum/n;
 }
- 
+
+int findAvgTurnaroundTime(int turnaroundTime[], int n){
+    int sum = 0;
+    int average = 0;
+    for (int i = 0; i < n; i++) {
+        sum += turnaroundTime[i];
+    }
+    return average = sum/n;
+}
+
+void swap(int* i, int* j){
+    int temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+int partition (int arr1[], int arr2[], int arr3[], int low, int high){
+    int piv = arr1[high]; 
+    int i = (low - 1);  
+    for (int j = low; j <= high- 1; j++){
+        if (arr1[j] <= piv){
+            i++; 
+            swap(&arr1[i], &arr1[j]);
+            swap(&arr2[i], &arr2[j]);
+            swap(&arr3[i], &arr3[j]);
+        }
+    }
+    swap(&arr1[i + 1], &arr1[high]);
+    swap(&arr2[i + 1], &arr2[high]);
+    swap(&arr3[i + 1], &arr3[high]);
+
+    return (i + 1);
+}
+
+void quickSort(int arr1[], int arr2[], int arr3[], int low, int high){
+    if (low < high){
+        int pi = partition(arr1, arr2, arr3, low, high);
+        quickSort(arr1, arr2, arr3, low, pi - 1);
+        quickSort(arr1, arr2, arr3, pi + 1, high);
+    }
+}
